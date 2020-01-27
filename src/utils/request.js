f94aa28fa3b005a133da2e15b980fe34703f5dda
baba <201694638@qq.com>
2020-01-27 22:33:11 +0800

    处理大数字

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,18 @@
 import axios from 'axios'
+import jsonBig from 'json-bigint' // 引入处理大数字
+
 // 默认的接口地址 使用复制一个axios方法
 const request = axios.crated({
   baseURL: 'http://ttapi.research.itcast.cn/'
 })
+// 处理大数字
+request.defaults.transformResponse = [function (data) {
+  try {
+    return jsonBig.parse(data)
+  } catch (err) {
+    return {}
+  }
+}]
 // 请求拦截器
 request.interceptors.request.use(function (config) {
   return config
@@ -19,4 +29,5 @@ request.interceptors.response.use(function (response) {
 function (error) {
   return Promise.reject(error)
 })
+
 export default request
