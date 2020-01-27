c48ff9791ce31717d19916ea326b701082df6af7
baba <201694638@qq.com>
2020-01-27 22:07:15 +0800

    rem适配

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue' // 跟组件
 import router from './router' // 路由 管理页面导航
 import store from './store' // 共享数据 容器
-import '@/utils/register'
+import '@/utils/register' // vant 按需引入组件
+import 'amfe-flexible' // 动态设置rem基准值
 Vue.config.productionTip = false
 
 new Vue({
