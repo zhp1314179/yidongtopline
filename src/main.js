bab7285daabc8420c8c24b7acfb42481f091e456
baba <201694638@qq.com>
2020-01-27 21:40:05 +0800

    引入vant 配置按需引入

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
+import App from './App.vue' // 跟组件
+import router from './router' // 路由 管理页面导航
+import store from './store' // 共享数据 容器
+import '@/utils/register'
 Vue.config.productionTip = false
 
 new Vue({
